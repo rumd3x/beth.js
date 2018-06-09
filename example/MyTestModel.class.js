29926e87f3f25e56288de8df960633d40ee9506f
rumd3x <edmurcardoso@gmail.com>
2018-06-09 13:52:12 -0300

    Updated example for new directives

--- example/MyTestModel.class.js
+++ example/MyTestModel.class.js
@@ -5,7 +5,12 @@ class MyTestModel {
 		this.tests = {
 			select: 1,
 			radio: 'female',
-			checkbox: true
+			checkbox: true,
+			options: [
+				{value: 0, text: 'Zero'}, 
+				{value: 1, text: 'One'}, 
+				{value: 2, text: 'Two'}
+			]
 		};
 	}
 
